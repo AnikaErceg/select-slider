e80ea92763f87e4ad975c7348b4a65f7dc82f04e
Anika Erceg <anika.erceg@modera.ee>
2020-04-22 19:18:21 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <base-test></base-test>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BaseTest from "./components/swipe-slider/BaseTest.vue"
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    BaseTest
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
